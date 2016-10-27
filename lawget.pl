fb8cfd633421dd3315291004f11150ff7cc8ff6a
John Oyler <john.m.oyler@gmail.com>
2016-10-26 21:33:21 -0500

    We to start from a higher level menu for testing.

--- lawget.pl
+++ lawget.pl
@@ -45,7 +45,7 @@ my $banner = "You can download statutory code, administrative code, law reporter
 print wrap('', '', $banner);
 
 # Sending them into the endless polling loop!
-while (my $module = menu('Texas')) {
+while (my $module = menu('United States')) {
     # Load up the module. Should only load once, even if called many times.
     load $module;
     # Configure is running every time, not sure if that's bad or not.
